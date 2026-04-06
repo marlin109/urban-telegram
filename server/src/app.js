dd15d207e8ac520845434884b726824b1b6838cb
marlin109 <SadJames071009@gmail.com>
2026-04-06 14:47:43 -0700

    add urlencoded middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
 
-
 const app = express();
 
+app.use(express.urlencoded({ extended: true }))
 
-app.listen(8080, () => {
-    console.log('listening on port 8080')
+
+app.get('/test', (req, res) => {
+    res.json(req.query)
+})
+
+app.listen(8081, () => {
+    console.log('listening on port 8081')
 })
