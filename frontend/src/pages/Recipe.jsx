28e291f181e7169da244a2220bbc40c9432ba8e9
marlin109 <SadJames071009@gmail.com>
2026-04-07 15:03:51 -0700

    add react router

--- frontend/src/pages/Recipe.jsx
+++ frontend/src/pages/Recipe.jsx
@@ -1,12 +1,15 @@
-
+import { useState } from "react"
 
 
 export default function Recipe() {
+    const [name, setName] = useState("Cheese Burger");
 
+
+    
 
     return (
         <main>
-            <h1>Today's Recipe</h1>
+            <h1>Today's Recipe: {name}</h1>
         </main>
     )
 }
